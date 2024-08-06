9d4c8e9b56099e0c2dddadb00609b0d866884020
bayashico0130 <kobayashi_naokii@yahoo.co.jp>
2024-08-07 00:05:09 +0900

    feat: todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-todo';
+  tasks = [
+    {title: '牛乳を買う', done: false},
+    {title: '可燃ゴミを出す', done: true},
+    {title: '銀行に行く', done: false},
+  ];
+  newTaskTitle = '';
+
+  addTask() {
+    this.tasks.push({title: this.newTaskTitle, done: false});
+    this.newTaskTitle = '';
+  }
 }
